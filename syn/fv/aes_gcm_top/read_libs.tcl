ff2d44d5973aaf8af1d7e66138e4e8f3c35d35cb
Yiyang Chen <yic079@ucsd.edu>
2025-10-17 15:54:51 -0700

    cleanup: stop tracking formal-verification artifacts (syn/fv)

--- syn/fv/aes_gcm_top/read_libs.tcl
+++
@@ -1,5 +0,0 @@
-add_search_path /opt/lib/tsmc/211005/tcbn40ulpbwp40_c170815_130d/TSMCHOME/digital/Front_End/timing_power_noise/NLDM/tcbn40ulpbwp40_c170815_130d -library -both
-read_library -liberty -both \
-    /opt/lib/tsmc/211005/tcbn40ulpbwp40_c170815_130d/TSMCHOME/digital/Front_End/timing_power_noise/NLDM/tcbn40ulpbwp40_c170815_130d/tcbn40ulpbwp40_c170815tt1p1v25c.lib \
-    /opt/lib/tsmc/211005/tcbn40ulpbwp40_c170815_130d/TSMCHOME/digital/Front_End/timing_power_noise/NLDM/tcbn40ulpbwp40_c170815_130d/tcbn40ulpbwp40_c170815tt1p1v25c.lib
-
